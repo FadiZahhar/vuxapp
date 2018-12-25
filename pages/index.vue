25d170aadba3e774dd08489ca56b29dc5e8891c3
Fadi Nicolas Zahhar <fnzahhar@gmail.com>
2018-12-25 02:20:19 +0200

    adding assets images and calling the background image

--- pages/index.vue
+++ pages/index.vue
@@ -43,6 +43,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
+  background-image:url(~assets/images/background-image.jpg);
   background-position: center;
   background-size: cover;
 }
